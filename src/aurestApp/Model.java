1dbb09235bd279dc24486fc554dadd97f26c1133
Garog <garog@ruhrnuklear.de>
2015-07-23 23:22:30 +0200

    v1.1.11 begonnen mit dem Suchfeld für den Bereich Kunden

--- src/aurestApp/Model.java
+++ src/aurestApp/Model.java
@@ -76,7 +76,7 @@ public class Model {
     }
 
     public String getVersion() {
-        return "1.1.10";
+        return "1.1.11";
     }
 
     public String getServicejahr() {
--- src/aurestApp/controller/EinstellungenKundenController.java
+++ src/aurestApp/controller/EinstellungenKundenController.java
@@ -5,13 +5,15 @@ import aurestApp.Tools.Kunde;
 import aurestApp.Tools.Settings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.paint.Color;
 import org.controlsfx.control.Notifications;
+import org.controlsfx.control.textfield.CustomTextField;
 import org.controlsfx.glyphfont.FontAwesome;
 import org.controlsfx.glyphfont.Glyph;
 
@@ -33,6 +35,8 @@ public class EinstellungenKundenController implements Initializable {
     private ObservableList<Kunde> kunden;
     private ArrayList<Kunde> zuLoeschend = new ArrayList<>();
     @FXML
+    private CustomTextField filterField;
+    @FXML
     private Button btnAddKunde;
     @FXML
     private Button btnDelKunde;
@@ -50,7 +54,13 @@ public class EinstellungenKundenController implements Initializable {
             btnAddKunde.setDisable(newValue.trim().isEmpty());
         });
 
-        setTableData();
+        kunden = FXCollections.observableArrayList(m.getKunden());
+
+        //Das asObject() muss hier leider dran, da aus Kompatibilitätsgründen der SimpleIntegerProperty ein Number und kein Integer liefert
+        //Es könnte auch die Tabelle auf Number geändert werden. könnte.. auch...
+        columnID.setCellValueFactory(cellData -> cellData.getValue().idProperty().asObject());
+
+        columnKunde.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
 
         btnAddKunde.setGraphic(new Glyph("FontAwesome", FontAwesome.Glyph.USER_PLUS).size(25.0).color(Color.GREEN));
         btnAddKunde.setContentDisplay(ContentDisplay.LEFT);
@@ -60,6 +70,41 @@ public class EinstellungenKundenController implements Initializable {
 
         btnSpeichern.setGraphic(new Glyph("FontAwesome", FontAwesome.Glyph.SAVE).size(25.0).color(Color.BLUE));
         btnSpeichern.setContentDisplay(ContentDisplay.LEFT);
+
+        filterField.setLeft(new Glyph("FontAwesome", FontAwesome.Glyph.SEARCH).color(Color.BLACK));
+        filterField.setPromptText("Suchen");
+
+        // 1. Wrap the ObservableList in a FilteredList (initially display all data).
+        FilteredList<Kunde> filteredData = new FilteredList<>(kunden, p -> true);
+
+        // 2. Set the filter Predicate whenever the filter changes.
+        filterField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(kunde -> {
+                // If filter text is empty, display all persons.
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+
+                // Compare first name and last name of every person with filter text.
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if (kunde.getName().toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter matches first name.
+                } else if (kunde.getName().toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter matches last name.
+                }
+                return false; // Does not match.
+            });
+        });
+
+        // 3. Wrap the FilteredList in a SortedList.
+        SortedList<Kunde> sortedData = new SortedList<>(filteredData);
+
+        // 4. Bind the SortedList comparator to the TableView comparator.
+        sortedData.comparatorProperty().bind(kundenTable.comparatorProperty());
+
+        // 5. Add sorted (and filtered) data to the table.
+        kundenTable.setItems(sortedData);
     }
 
     @FXML
@@ -79,7 +124,7 @@ public class EinstellungenKundenController implements Initializable {
             }
         }
         //wenn alles OK, dann füge ihn der Tabelle!! hinzu, erst mit dem Speichern wird es übernommen
-        kundenTable.getItems().add(new Kunde(0, txtAddKunde.getText()));
+        kunden.add(new Kunde(0, txtAddKunde.getText()));
     }
 
     @FXML
@@ -100,7 +145,7 @@ public class EinstellungenKundenController implements Initializable {
             Kunde currentPerson = kundenTable.getSelectionModel().getSelectedItem();
             //remove selected item from the table list
             zuLoeschend.add(currentPerson);
-            kundenTable.getItems().remove(currentPerson);
+            kunden.remove(currentPerson);
         }
     }
 
@@ -108,17 +153,5 @@ public class EinstellungenKundenController implements Initializable {
     private void handelSpeicherKunden(ActionEvent actionEvent) {
         m.setZuLoeschendenKunden(zuLoeschend);
         Settings.speicherKunden(m, kunden);
-    }
-
-    private void setTableData() {
-        kunden = FXCollections.observableArrayList(m.getKunden());
-
-        //Das asObject() muss hier leider dran, da aus Kompatibilitätsgründen der SimpleIntegerProperty ein Number und kein Integer liefert
-        //Es könnte auch die Tabelle auf Number geändert werden. könnte.. auch...
-        columnID.setCellValueFactory(cellData -> cellData.getValue().idProperty().asObject());
-
-        columnKunde.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
-
-        kundenTable.setItems(kunden);
     }
 }
