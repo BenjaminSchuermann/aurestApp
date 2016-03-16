7941618120da60d86b9cce40611c3ada82eed68b
Garog <garog@ruhrnuklear.de>
2016-03-16 15:10:43 +0100

    Begonnen den Style in CSS auszulagern und Update Logo

--- src/aurestApp/MainController.java
+++ src/aurestApp/MainController.java
@@ -1,15 +1,12 @@
 package aurestApp;
 
 import aurestApp.controller.*;
-import aurestApp.interfaces.Images;
 import aurestApp.interfaces.Seiten;
-import aurestApp.tools.eigeneklassen.GetImageView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -91,32 +88,18 @@ class MainController implements Seiten, Initializable {
 
 //Hauptleiste anlegen
         bt1.setUserData("Projekt");
-        bt1.setGraphic(GetImageView.load(Images.APPLICATION_FROM_STORAGE, 32));
-        bt1.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
 
         bt2.setUserData("Service");
-        bt2.setGraphic(GetImageView.load(Images.WRENCH, 32));
-        bt2.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
 
         bt3.setUserData("eMails");
-        bt3.setGraphic(GetImageView.load(Images.EMAILS_STACK, 32));
-        bt3.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
 
         bt4.setUserData("Einstellungen");
-        bt4.setGraphic(GetImageView.load(Images.COG, 32));
-        bt4.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
 
         bt5.setUserData("Info");
-        bt5.setGraphic(GetImageView.load(Images.INFO_RHOMBUS, 32));
-        bt5.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
 
         bt6.setUserData("Beenden");
-        bt6.setGraphic(GetImageView.load(Images.CANCEL, 32));
-        bt6.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
 
         bt7.setUserData("Offerte");
-        bt7.setGraphic(GetImageView.load(Images.BLUEPRINT, 32));
-        bt7.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
 
         sb1.getButtons().addAll(bt1, bt2, bt7, bt3, bt4, bt5, bt6);
         sb1.getStyleClass().add(SegmentedButton.STYLE_CLASS_DARK);
@@ -156,17 +139,10 @@ class MainController implements Seiten, Initializable {
 
         //Leiste Projekt anlegen
         pbt1.setUserData("Projekt anlegen");
-        pbt1.setGraphic(GetImageView.load(Images.APPLICATION_FORM_ADD, 16));
-
-        pbt1.setContentDisplay(ContentDisplay.LEFT);
 
         pbt2.setUserData("Projekt Logbuch");
-        pbt2.setGraphic(GetImageView.load(Images.REPORT_ADD, 16));
-        pbt2.setContentDisplay(ContentDisplay.LEFT);
 
         pbt3.setUserData("Offene Projekte");
-        pbt3.setGraphic(GetImageView.load(Images.APPLICATION_VIEW_LIST, 16));
-        pbt3.setContentDisplay(ContentDisplay.LEFT);
 
         //Zum Entwicklen nur meinem Login den dritten Button anzeigen
         if (m.getUserid() != 1) {
@@ -180,36 +156,22 @@ class MainController implements Seiten, Initializable {
 
         //Leiste Service anlegen
         sbt1.setUserData("Service anlegen");
-        sbt1.setGraphic(GetImageView.load(Images.WRENCHADD, 16));
-        sbt1.setContentDisplay(ContentDisplay.LEFT);
 
         sbt2.setUserData("Service Logbuch");
-        sbt2.setGraphic(GetImageView.load(Images.REPORT_ADD, 16));
-        sbt2.setContentDisplay(ContentDisplay.LEFT);
 
         ssb.getButtons().addAll(sbt1, sbt2);
         ssb.getStyleClass().add(SegmentedButton.STYLE_CLASS_DARK);
 
         //Leiste Einstellungen anlegen
         ebt1.setUserData("Mitarbeiter");
-        ebt1.setGraphic(GetImageView.load(Images.USER_EDIT, 16));
-        ebt1.setContentDisplay(ContentDisplay.LEFT);
 
         ebt2.setUserData("Servicejahr");
-        ebt2.setGraphic(GetImageView.load(Images.CALENDAR_EDIT, 16));
-        ebt2.setContentDisplay(ContentDisplay.LEFT);
 
         ebt3.setUserData("Ordnervorlagen");
-        ebt3.setGraphic(GetImageView.load(Images.FOLDER_EDIT, 16));
-        ebt3.setContentDisplay(ContentDisplay.LEFT);
 
         ebt4.setUserData("Kunden");
-        ebt4.setGraphic(GetImageView.load(Images.VCARD_EDIT, 16));
-        ebt4.setContentDisplay(ContentDisplay.LEFT);
 
         ebt5.setUserData("Logindaten");
-        ebt5.setGraphic(GetImageView.load(Images.LOCK_EDIT, 16));
-        ebt5.setContentDisplay(ContentDisplay.LEFT);
 
         sb2.getButtons().addAll(ebt1, ebt2, ebt3, ebt4, ebt5);
         sb2.getStyleClass().add(SegmentedButton.STYLE_CLASS_DARK);
